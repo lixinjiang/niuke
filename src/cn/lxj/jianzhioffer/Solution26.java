4d39cbea2d257b8e545356f0d9397a95f44f4d72
lixinjiang <1185860710@qq.com>
2018-09-11 12:43:47 +0800

    update

--- src/cn/lxj/jianzhioffer/Solution26.java
+++ src/cn/lxj/jianzhioffer/Solution26.java
@@ -7,8 +7,26 @@ package cn.lxj.jianzhioffer;
  * create class by lxj 2018/9/10
  **/
 public class Solution26 {
-    public TreeNode Convert(TreeNode pRootOfTree) {
+    // 采用中序遍历后的节点是有序的，所以采用的遍历顺序应该是中序的
+    public TreeNode Convert(TreeNode pRootOfTree) {// 根节点？
+        TreeNode lastNode = null;
+        TreeNode headNode = ConvertNode(pRootOfTree,lastNode);
+        while (headNode != null && headNode.left != null)
+            headNode = headNode.left;
+        return headNode;
+    }
 
-        return null;
+    public TreeNode ConvertNode(TreeNode rootTree,TreeNode lastNode){
+        if (rootTree == null)
+            return null;
+        if (rootTree.left != null)//一直找到最左节点
+            lastNode = ConvertNode(rootTree.left, lastNode);
+        rootTree.left = lastNode;
+        if (lastNode != null)
+            lastNode.right = rootTree;
+        lastNode = rootTree;
+        if (rootTree.right!=null)
+            lastNode = ConvertNode(rootTree.right, lastNode);
+        return lastNode;
     }
 }
